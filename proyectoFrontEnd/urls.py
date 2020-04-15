2c3b9c314de8f3c19f288c2824315ab329df9527
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2020-04-15 16:58:48 -0500

    Finished

--- proyectoFrontEnd/urls.py
+++ proyectoFrontEnd/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ultrasonic import views as ultrasonic_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ultrasonic_views.ultrasonic, name='ultrasonic')
 ]
